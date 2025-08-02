a7d5230fe29d2bfdca44e1d170beee2aa5607de9
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-08-03 04:00:18 +0530

    new app

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      
+      <Text>Scan the qr code using the camera app instead of the expo go app. It works then only</Text>
+    </View>
+  );
+}
