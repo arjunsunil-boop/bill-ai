667d8bdc473f2120bf689a437fd116232897a2df
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-08-13 15:01:09 +0530

    add card data

--- app/(tabs)/billsPage.tsx
+++ app/(tabs)/billsPage.tsx
@@ -1,36 +1,52 @@
 import SearchButton from '@/components/SearchButton';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-    import { Image } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+
 export default function BillsPage() {
-  const [bills,setBills]=useState(
-    [
-      {
-        "img":"assets/images/wallhaven-e8zwwo_2560x1600.png"
-      },
-    ]
-  )
+  const [bills, setBills] = useState([
+    {
+      img: "https://as1.ftcdn.net/v2/jpg/01/82/01/18/1000_F_182011806_mxcDzt9ckBYbGpxAne8o73DbyDHpXOe9.jpg",
+    }, {
+      img: "https://t3.ftcdn.net/jpg/02/49/45/18/240_F_249451824_0KPU9PF3Re03Zpq0bNRSuG7Kr3GSnwse.webp",
+    }, {
+      img: "https://as1.ftcdn.net/v2/jpg/01/82/01/18/1000_F_182011806_mxcDzt9ckBYbGpxAne8o73DbyDHpXOe9.jpg",
+    }
+  ])
   return (
     <View style={styles.container}>
 
       <View style={styles.searchContainer}>
 
         <TextInput style={styles.textInput} />
-        <SearchButton/>
+        <SearchButton />
       </View>
-      <View>
-        <View style={styles.card}>
-          <View style={styles.cardImageContainer}>
-            <Image source={require('@/assets/images/wallhaven-e8zwwo_2560x1600.png')} style={{width:"100%",height:"100%",}}/>
+      <View style={styles.cardContainer}>
+        {
+          bills.map(
+            (bill) => {
+              return (
 
-          </View>
-          <View>
-            <Text>
-              Bill Details
-            </Text>
-          </View>
-          
-        </View>
+                <Pressable onPress={(bill)=>{alert(`You pressed ${bill}`)}}>
+
+                  <View style={styles.card}>
+                    <View style={styles.cardImageContainer}>
+                      <Image source={{ uri: bill.img }} style={{ width: "100%", height: "100%", }} />
+
+                    </View>
+                    <View>
+                      <Text>
+                        Bill Details
+                      </Text>
+                    </View>
+
+                  </View>
+
+                </Pressable>
+
+              )
+            }
+          )
+        }
       </View>
 
 
@@ -63,25 +79,29 @@ const styles = StyleSheet.create(
       borderRadius: 5,
       width: 250,
     },
-    card:{
-      width:150,
-      height:250,
-      backgroundColor:"#fff",
-      borderColor:"#000",
-      borderWidth:2,
-      borderRadius:10,
-      margin:20,
-      justifyContent:"center",
-      alignItems:"center"
+    card: {
+      width: 150,
+      height: 250,
+      backgroundColor: "#fff",
+      borderColor: "#000",
+      borderWidth: 2,
+      borderRadius: 10,
+      margin: 20,
+      justifyContent: "center",
+      alignItems: "center"
     },
-    cardImageContainer:{
-      width:130,
-      height:200,
-      alignItems:"center",
-      justifyContent:"center",
-      borderRadius:10,
-      borderWidth:2
+    cardImageContainer: {
+      width: "100%",
+      height: 180,
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: 10,
+      borderWidth: 0,
+      overflow: "hidden"
 
+    },
+    cardContainer:{
+      
     }
 
   }
---
+++ components/BillDetails.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function BillDetails(){
+
+    return (
+        <View>
+            <Text>This is the bill details model</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create(
+
+    {
+        ModalContainer:{
+
+    }
+
+}
+)
